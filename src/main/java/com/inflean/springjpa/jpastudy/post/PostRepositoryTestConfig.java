dbcffe8705d01d0fdfd8e4a71738dbd2f31effa2
windjay <windjay@daum.net>
2019-01-27 17:29:33 +0900

    도메인이벤트 변경

--- src/main/java/com/inflean/springjpa/jpastudy/post/PostRepositoryTestConfig.java
+++ src/main/java/com/inflean/springjpa/jpastudy/post/PostRepositoryTestConfig.java
@@ -1,12 +1,19 @@
 package com.inflean.springjpa.jpastudy.post;
 
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PostRepositoryTestConfig {
   @Bean
-  public PostListener postListener() {
-    return new PostListener();
+  public ApplicationListener<PostPublishedEvent> postListener() {
+    return event -> {
+      System.out.println("------------------------------");
+      System.out.println(event.getPost().getTitle() + " is published");
+
+    };
+
+//    return new PostListener();
   }
 }
