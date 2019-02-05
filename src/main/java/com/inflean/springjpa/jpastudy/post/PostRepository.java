8191bd5689c0319b91befa659579b5cd89a10b28
windjay <windjay@daum.net>
2019-02-06 05:02:56 +0900

    이벤트 퍼블리싱 관련 코드에 커멘트 등을 처리함~

--- src/main/java/com/inflean/springjpa/jpastudy/post/PostRepository.java
+++ src/main/java/com/inflean/springjpa/jpastudy/post/PostRepository.java
@@ -4,5 +4,5 @@ import com.inflean.springjpa.jpastudy.MyRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface PostRepository  extends MyRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
-
+  // querydsl 인터페이스 상속함
 }
